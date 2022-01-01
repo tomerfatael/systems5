f8bef116dfdac28d720e78830711bc055c2db9aa
tomerfatael <tomerfatael@mail.tau.ac.il>
2022-01-01 19:38:21 +0200

    added test

--- pcc_client.c
+++ pcc_client.c
@@ -57,7 +57,7 @@ int main(int argc, char** argv) {
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
-    if( (inet_pton(AF_INET, ip, &serv_addr.sin_addr)) != 1 ) {//need to check about it!!!!!!
+    if( (inet_pton(AF_INET, ip, &serv_addr.sin_addr)) != 1 ) {
         perror("inet_pton failed\n");
         exit(1);
     } 
